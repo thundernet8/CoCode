8902088d3356fd8d0903b09e73472e64649d36e6
WuXueqian <wuxueqian2010@hotmail.com>
2015-10-31 09:59:15 +0800

    AppIcon and Some Setting

---
+++ CoCode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoCode
+//
+//  Created by wuxueqian on 15/10/30.
+//  Copyright (c) 2015年 wuxueqian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CoCodeAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CoCodeAppDelegate class]));
+    }
+}
